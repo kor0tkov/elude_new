bbefbfa4d35faccf44194900546ec8a287eb1acc
Konstantin <quanoxy@gmail.com>
2019-12-17 23:34:57 +0500

    add scss, csv-parser

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="app" id="app">
+    <img alt="Vue logo" src="./assets/logo.png" class="app__logo">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
+<style lang="scss">
+.app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
